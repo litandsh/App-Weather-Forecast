2f2684b96ea60a929c3b3536024b1d83cf97a1a4
Andrii Litviniuk <litandsh@gmail.com>
2022-10-04 14:31:57 +0200

    feat: implement request to API Weather

--- script.js
+++ script.js
@@ -7,7 +7,30 @@ function getWeather() {
     const cityId = document.querySelector('#city').value;
     fetch(`${param.url}weather?id=${cityId}&appid=${param.appid}`)
         .then(weather => weather.json())
-    .then(showWeather)
+        .then(showWeather)
+    
+    //London, Uk
+    if (cityId == 2643743) {
+        document.querySelector('.app-back').classList.remove('app-back_LA', 'app-back_Shp', 'app-back_SPdP')
+        document.querySelector('.app-back').className += " app-back_London";
+    }
+
+    //Los Angeles, US
+    if (cityId == 5368361) {
+        document.querySelector('.app-back').classList.remove('app-back_London', 'app-back_Shp', 'app-back_SPdP')
+        document.querySelector('.app-back').className += " app-back_LA";
+    }
+
+    //Shepetivka, UA (UA - brave country)
+    if (cityId == 694216) {
+        document.querySelector('.app-back').classList.remove('app-back_London', 'app-back_LA', 'app-back_SPdP')
+        document.querySelector('.app-back').className += " app-back_Shp";
+    }
+
+    if (cityId == 6359540) {
+        document.querySelector('.app-back').classList.remove('app-back_London', 'app-back_LA', 'app-back_Shp')
+        document.querySelector('.app-back').className += " app-back_SPdP";
+    }
 }
 
 function showWeather(data) {
