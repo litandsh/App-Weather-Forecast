deee1204947737f103d84dd2023928193fa89c08
Andrii Litviniuk <litandsh@gmail.com>
2022-10-08 11:45:04 +0200

    refactor: change markup block 'selectCity' implement it by JS

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let selectCity = document.createElement('select');
+selectCity.setAttribute('id', 'city');
+
+let optionSelect1 = document.createElement('option');
+optionSelect1.setAttribute('value', '2643743');
+optionSelect1.textContent = 'London, UK';
+
+let optionSelect2 = document.createElement('option');
+optionSelect2.setAttribute('value', '6359540');
+optionSelect2.textContent = 'San Pedro del Pinatar, ES';
+
+let optionSelect3 = document.createElement('option');
+optionSelect3.setAttribute('value', '5368361');
+optionSelect3.textContent = 'Los Angeles, US';
+
+let optionSelect4 = document.createElement('option');
+optionSelect4.setAttribute('value', '694216');
+optionSelect4.textContent = 'Shepetivka, UA';
+
+
+selectCity.append(optionSelect1);
+selectCity.append(optionSelect2);
+selectCity.append(optionSelect3);
+selectCity.append(optionSelect4);
+
+document.querySelector('.container_citySelect').appendChild(selectCity);
+
 const param = {
     "url": "https://api.openweathermap.org/data/2.5/",
     "appid" : "a495189585a821e018d7f6f2e9e3ab77"
